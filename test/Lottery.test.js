a5f26f798fb9756fc9365ae42de96c1797efe8de
Bung Danar <danar.g.putera@gmail.com>
2023-03-19 10:07:02 +0700

    Update libs

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert')
 const ganache = require('ganache-cli')
 const Web3 = require('web3')
-const { interface, bytecode } = require('../compile')
+const { abi, evm } = require('../compile')
 
 const web3 = new Web3(ganache.provider())
 
@@ -13,8 +13,8 @@ const MAX_MULTIPLE_ACCOUNTS = 3
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts()
 
-  lottery = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
+  lottery = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
     .send({ from: accounts[0], gas: '1000000' })
 })
 
